65e7655bf9128ee5e8c1193b146a14185872f283
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-06-13 10:51:19 -0300

    feat: create useEditProfileForm hook

---
+++ src/hooks/useEditProfileForm.ts
@@ -0,0 +1,38 @@
+import { schemas } from "@/lib/zod/schemas";
+import { EditProfileForm } from "@/types/EditProfileForm";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+export default function useEditProfileForm() {
+  const defaultValues = {
+    company: "",
+    cnpj: "",
+    owner: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EditProfileForm>({
+    resolver: zodResolver(schemas.editProfile),
+    defaultValues,
+  });
+
+  const handleSubmitEditProfileForm: SubmitHandler<EditProfileForm> = (
+    data
+  ) => {
+    console.log(data);
+  };
+
+  const onSubmit = handleSubmit(handleSubmitEditProfileForm);
+
+  return {
+    errors,
+    onSubmit,
+    register,
+  };
+}
