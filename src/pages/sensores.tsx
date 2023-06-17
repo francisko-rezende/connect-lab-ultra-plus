18b43bee748b8841066ba426e000380a5bf7e5b8
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-06-17 18:27:57 -0300

    feat: create sensors data table

--- src/pages/sensores.tsx
+++ src/pages/sensores.tsx
@@ -1,9 +1,10 @@
 import { ReactElement } from "react";
 import type { NextPageWithLayout } from "./_app";
 import { LoggedInLayout } from "@/layouts/LoggedInLayout";
+import { Sensors } from "@/templates/Sensors";
 
 const Page: NextPageWithLayout = () => {
-  return <h1>Sensores</h1>;
+  return <Sensors />;
 };
 
 Page.getLayout = function getLayout(page: ReactElement) {
