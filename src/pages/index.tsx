d84a58940bcfc099959d46c00e042fb419972c89
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-06-17 18:26:50 -0300

    feat: start using SectionTitle in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { ReactElement } from "react";
 import type { NextPageWithLayout } from "./_app";
 import { LoggedInLayout } from "@/layouts/LoggedInLayout";
+import { SectionTitle } from "@/components/SectionTitle/SectionTitle";
 
 const Page: NextPageWithLayout = () => {
   return (
     <>
-      <h2 className="text-xl font-semibold">Campo de Soja</h2>
+      <SectionTitle>Campo de Soja</SectionTitle>
       <section className=" grid max-w-[1373px] grid-cols-overview grid-rows-[repeat(auto-fill,_325px)] gap-5">
         <div className="h-[325px] border-[1.5px] border-gray-200 bg-fuchsia-300"></div>
         <div className="h-[325px] border-[1.5px] border-gray-200 bg-fuchsia-300"></div>
