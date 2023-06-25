1fbd408c627bd64ccf8bab7e419381e10801ca74
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-06-25 20:36:20 -0300

    feat: update Card so it has variants for the different card types

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,47 @@
-type CardProps = {
-  children: React.ReactNode;
-};
+import { cva, type VariantProps } from "class-variance-authority";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
-export function Card({ children }: CardProps) {
+const cardStyles = cva(
+  [
+    "h-[325px]",
+    "border-[1.5px]",
+    "border-gray-200",
+    "rounded-xl",
+    "shadow-md",
+    "overflow-hidden",
+  ],
+  {
+    variants: {
+      hasPadding: {
+        true: ["px-7", "py-8"],
+      },
+      variant: {
+        map: ["flex", "flex-col", "justify-between", "gap-4"],
+      },
+    },
+    defaultVariants: {
+      hasPadding: true,
+    },
+  }
+);
+
+export type CardProps = ComponentProps<"article"> &
+  VariantProps<typeof cardStyles>;
+
+export function Card({
+  variant,
+  children,
+  hasPadding,
+  className,
+  ...props
+}: CardProps) {
   return (
-    <section className="mx-auto w-full max-w-[480px] rounded bg-white px-12 py-16">
+    <article
+      className={twMerge(cardStyles({ hasPadding, variant }), className)}
+      {...props}
+    >
       {children}
-    </section>
+    </article>
   );
 }
