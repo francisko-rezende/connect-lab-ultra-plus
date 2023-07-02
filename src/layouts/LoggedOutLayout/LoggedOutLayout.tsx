c0263f2c56ccdd01c78f745ea8f15ba15a544340
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-07-02 11:30:49 -0300

    feat: add toaster to LoggedOutLayout

--- src/layouts/LoggedOutLayout/LoggedOutLayout.tsx
+++ src/layouts/LoggedOutLayout/LoggedOutLayout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,10 +9,13 @@ type LoggedOutLayoutProps = {
 
 export function LoggedOutLayout({ children }: LoggedOutLayoutProps) {
   return (
-    <main
-      className={`grid h-full w-full items-center bg-blue-50 px-2 ${inter.className} overflow-auto py-7`}
-    >
-      {children}
-    </main>
+    <>
+      <main
+        className={`grid h-full w-full items-center bg-blue-50 px-2 ${inter.className} overflow-auto py-7`}
+      >
+        {children}
+      </main>
+      <Toaster />
+    </>
   );
 }
