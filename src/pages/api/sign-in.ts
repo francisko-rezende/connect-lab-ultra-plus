daf5e11c000bad968a67eac63050e0ba0ca689f6
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-07-09 09:43:55 -0300

    feat: create api route for logging in

---
+++ src/pages/api/sign-in.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/lib/prisma/prismaClient";
+import { NextApiRequest, NextApiResponse } from "next";
+import * as bcrypt from "bcrypt";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { password, email } = req.body;
+
+  const companyData = await prisma.company.findFirst({ where: { email } });
+
+  if (!companyData) {
+    res.json(null);
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(password, companyData.salt);
+  const isPasswordCorrect = companyData.password === hashedPassword;
+
+  if (!isPasswordCorrect) {
+    res.json(null);
+    return;
+  }
+
+  const { companyId, company, email: savedEmail } = companyData;
+  return res
+    .status(200)
+    .json({ id: companyId, name: company, email: savedEmail });
+}
