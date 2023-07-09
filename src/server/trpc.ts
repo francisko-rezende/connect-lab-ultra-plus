1ae89d49be152fd93847bf97397d0e74e05ca329
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-07-09 09:52:47 -0300

    feat: create authed procedure

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,11 +1,40 @@
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
+import { Context } from "./context";
 
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create();
+const t = initTRPC.context<Context>().create();
 
 // Base router and procedure helpers
+
+const isAuthed = t.middleware(({ next, ctx }) => {
+  const user = ctx.session?.user;
+
+  // console.log({ ctx });
+
+  if (!user?.name) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({
+    // ctx: {
+    //   user: {
+    //     ...user,
+    //     name: user.name,
+    //   },
+    // },
+    ctx,
+  });
+});
+
 export const router = t.router;
 export const procedure = t.procedure;
+
+// export const middleware = t.middleware;
+
+/**
+ * Protected base procedure
+ */
+export const authedProcedure = t.procedure.use(isAuthed);
