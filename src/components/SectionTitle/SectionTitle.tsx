373e0e245b0fa7f343fd8b7e81966003e42c58b0
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-07-09 19:07:45 -0300

    feat: adjust section title style

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -3,5 +3,7 @@ import { ComponentProps } from "react";
 type SectionTitleProps = ComponentProps<"h2">;
 
 export const SectionTitle = ({ children }: SectionTitleProps) => {
-  return <h2 className="mb-20 font-semibold">{children}</h2>;
+  return (
+    <h2 className="mb-20 text-lg font-semibold text-stone-900">{children}</h2>
+  );
 };
