070ade8a2e82d7eb8fb7245a31ab48d15eec29a6
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-07-09 19:09:25 -0300

    feat: move data table code to Locations and Sensors layouts

--- src/templates/Locations.tsx
+++ src/templates/Locations.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/Button";
 import { Checkbox } from "@/components/Checkbox/Checkbox";
-import { DataTable } from "@/components/DataTable/DataTable";
+import { CreateLocationDialog } from "@/components/CreateLocationDialog";
 import { SectionTitle } from "@/components/SectionTitle/SectionTitle";
-import { ColumnDef } from "@tanstack/react-table";
-import { useMemo } from "react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { Edit } from "lucide-react";
+import { useMemo, useState } from "react";
 
 type Location = {
   id: number;
@@ -95,14 +103,121 @@ export function Locations() {
         accessorKey: "installedSensorsNumber",
         header: "Sensores",
       },
+      {
+        id: "edit",
+        header: "Editar",
+        cell: (info) => {
+          return (
+            <button onClick={() => alert(JSON.stringify(info.row.original))}>
+              <Edit className="transition-opacity hover:opacity-60" />
+              <span className="sr-only">Editar</span>
+            </button>
+          );
+        },
+      },
     ],
     []
   );
 
+  const [searchedTerm, setSearchedTerm] = useState("");
+  const [rowSelection, setRowSelection] = useState({});
+
+  const table = useReactTable({
+    data: locations,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onGlobalFilterChange: setSearchedTerm,
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    enableRowSelection: true,
+    state: {
+      globalFilter: searchedTerm,
+      rowSelection,
+    },
+  });
+
   return (
     <>
-      <SectionTitle>Locations</SectionTitle>
-      <DataTable columns={columns} data={locations} />
+      <SectionTitle>Locais</SectionTitle>
+      <div>
+        <div className="mb-16 flex items-center justify-between">
+          <div className="relative flex items-center">
+            <svg
+              className="absolute"
+              width="24"
+              height="24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="11"
+                cy="11.5"
+                r="6.5"
+                stroke="#9395A3"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M15.519 16.485 19.043 20"
+                stroke="#9395A3"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <input
+              className="max-w-[350px] flex-1 rounded border-none pl-8"
+              type="search"
+              placeholder="Digite para filtrar os dados pelo nome..."
+              value={table.getState().globalFilter}
+              onChange={(e) => table.setGlobalFilter(e.target.value)}
+            />
+          </div>
+          <div className="space-x-8">
+            <Button variant={"noBorder"}>Excluir</Button>
+            <CreateLocationDialog
+              trigger={<Button variant={"primary"}>Novo Local</Button>}
+            />
+          </div>
+        </div>
+        <div className="">
+          <table className=" w-full">
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr className="border-b-2 border-gray-300" key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th className="py-5 font-medium" key={header.id}>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map((row) => (
+                <tr
+                  className="border-b-2 border-gray-300 last-of-type:border-none"
+                  key={row.id}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <td key={cell.id} className="py-5 text-center">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {/* <DataTable columns={columns} data={locations} /> */}
     </>
   );
 }
--- src/templates/Sensors.tsx
+++ src/templates/Sensors.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/Button";
 import { Checkbox } from "@/components/Checkbox/Checkbox";
-import { DataTable } from "@/components/DataTable/DataTable";
+import { LinkSensorDialog } from "@/components/LinkSensorDialog";
 import { SectionTitle } from "@/components/SectionTitle/SectionTitle";
-import { ColumnDef } from "@tanstack/react-table";
-import { useMemo } from "react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useMemo, useState } from "react";
+import { Edit } from "lucide-react";
 
 type Sensor = {
   id: number;
@@ -73,6 +81,7 @@ export function Sensors() {
       {
         accessorKey: "name",
         header: "Nome",
+        enableGlobalFilter: true,
       },
       {
         accessorKey: "createdAt",
@@ -88,10 +97,22 @@ export function Sensors() {
         accessorKey: "status",
         header: "Status",
         cell: (info) => {
-          return info.getValue() ? (
+          return info.getValue<boolean>() ? (
             <span className="font-semibold text-green-600">Ativo</span>
           ) : (
             <span className="font-semibold text-red-500">Inativo</span>
+          );
+        },
+      },
+      {
+        id: "edit",
+        header: "Editar",
+        cell: (info) => {
+          return (
+            <button onClick={() => alert(info.row.original.id)}>
+              <Edit className="transition-opacity hover:opacity-60" />
+              <span className="sr-only">Editar</span>
+            </button>
           );
         },
       },
@@ -99,10 +120,104 @@ export function Sensors() {
     []
   );
 
+  const [searchedTerm, setSearchedTerm] = useState("");
+  const [rowSelection, setRowSelection] = useState({});
+
+  const table = useReactTable({
+    data: sensors,
+    columns: locationColumns,
+    getCoreRowModel: getCoreRowModel(),
+    onGlobalFilterChange: setSearchedTerm,
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    enableRowSelection: true,
+    state: {
+      globalFilter: searchedTerm,
+      rowSelection,
+    },
+  });
+
   return (
     <>
-      <SectionTitle>Locations</SectionTitle>
-      <DataTable columns={locationColumns} data={sensors} />
+      <SectionTitle>Sensores</SectionTitle>
+      <div>
+        <div className="mb-16 flex items-center justify-between">
+          <div className="relative flex items-center">
+            <svg
+              className="absolute"
+              width="24"
+              height="24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="11"
+                cy="11.5"
+                r="6.5"
+                stroke="#9395A3"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M15.519 16.485 19.043 20"
+                stroke="#9395A3"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <input
+              className="max-w-[350px] flex-1 rounded border-none pl-8"
+              type="search"
+              placeholder="Digite para filtrar os dados pelo nome..."
+              value={table.getState().globalFilter}
+              onChange={(e) => table.setGlobalFilter(e.target.value)}
+            />
+          </div>
+          <div className="space-x-8">
+            <Button variant={"noBorder"}>Excluir</Button>
+            <LinkSensorDialog
+              trigger={<Button variant={"primary"}>Novo Sensor</Button>}
+            />
+          </div>
+        </div>
+        <div className="">
+          <table className=" w-full">
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr className="border-b-2 border-gray-300" key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th className="py-5 font-medium" key={header.id}>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map((row) => (
+                <tr
+                  className="border-b-2 border-gray-300 last-of-type:border-none"
+                  key={row.id}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <td key={cell.id} className="py-5 text-center">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
