5a006cf4a0171df916cd779554d401117ba32852
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-07-29 11:56:27 -0300

    feat: use getServerSideProps to redirect logged in user from login and registration pages

--- src/pages/cadastro.tsx
+++ src/pages/cadastro.tsx
@@ -2,6 +2,8 @@ import { ReactElement } from "react";
 import type { NextPageWithLayout } from "./_app";
 import { LoggedOutLayout } from "@/layouts/LoggedOutLayout";
 import { SignUp } from "@/templates/SignUp";
+import { getSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
 
 const SignUpPage: NextPageWithLayout = () => {
   return <SignUp />;
@@ -9,6 +11,23 @@ const SignUpPage: NextPageWithLayout = () => {
 
 SignUpPage.getLayout = function getLayout(page: ReactElement) {
   return <LoggedOutLayout>{page}</LoggedOutLayout>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
+
+  if (!!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default SignUpPage;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,8 @@ import { ReactElement } from "react";
 import type { NextPageWithLayout } from "./_app";
 import { LoggedOutLayout } from "@/layouts/LoggedOutLayout";
 import { SignIn } from "@/templates/SignIn";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
 
 const Page: NextPageWithLayout = () => {
   return <SignIn />;
@@ -9,6 +11,23 @@ const Page: NextPageWithLayout = () => {
 
 Page.getLayout = function getLayout(page: ReactElement) {
   return <LoggedOutLayout>{page}</LoggedOutLayout>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
+
+  if (!!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default Page;
