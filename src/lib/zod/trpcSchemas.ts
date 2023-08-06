ade5b68c7049995ad25e8a5c983e4fb30bf0c6d6
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-08-06 20:39:08 -0300

    feat: add trpc schemas for linkSensor and getSensors procedures

--- src/lib/zod/trpcSchemas.ts
+++ src/lib/zod/trpcSchemas.ts
@@ -1,6 +1,10 @@
 import * as z from "zod";
 import { schemas } from "./schemas";
 
+const locationId = z.number();
+
 export const trpcSchemas = {
   updateLocation: schemas.createLocation.extend({ locationId: z.number() }),
+  getSensors: z.object({ locationId }),
+  linkSensor: z.object({ sensorData: schemas.linkSensor, locationId }),
 };
