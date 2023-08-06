75b1857d5261217dce5e9a9aec61e27f4c1b0f49
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-08-06 11:09:50 -0300

    feat: create RetainQueryLink

---
+++ src/components/RetainQueryLink/RetainQueryLink.tsx
@@ -0,0 +1,30 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { PropsWithChildren } from "react";
+
+export const RetainQueryLink = ({
+  href,
+  ...props
+}: LinkProps & PropsWithChildren & { className: string }) => {
+  const router = useRouter();
+
+  const pathname = typeof href === "object" ? href.pathname : href;
+
+  const query =
+    typeof href === "object" && typeof href.query === "object"
+      ? href.query
+      : {};
+
+  return (
+    <Link
+      {...props}
+      href={{
+        pathname: pathname,
+        query: {
+          ...router.query,
+          ...query,
+        },
+      }}
+    />
+  );
+};
